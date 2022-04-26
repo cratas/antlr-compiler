24c908e128c62087ae75e7c78b48cf06ef69505a
cratas <petr.kratochvil.st1@vsb.cz>
2022-04-26 12:55:15 +0200

    first commit

---
+++ src/gListener.java
@@ -0,0 +1,19 @@
+// Generated from C:/Users/Ryzen 5/Desktop/School/6th_semester/PJP/Compiler\g.g4 by ANTLR 4.10.1
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link gParser}.
+ */
+public interface gListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link gParser#prule}.
+	 * @param ctx the parse tree
+	 */
+	void enterPrule(gParser.PruleContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link gParser#prule}.
+	 * @param ctx the parse tree
+	 */
+	void exitPrule(gParser.PruleContext ctx);
+}
